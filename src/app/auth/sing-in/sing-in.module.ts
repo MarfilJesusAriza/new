6105c71fca72f6db6008f376422f04f0f9ed5406
Jesus Ariza <ilvarjesus@hotmail.com>
2025-04-21 21:04:31 -0500

    crecion de los modulos

---
+++ src/app/auth/sing-in/sing-in.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SingInRoutingModule } from './sing-in-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SingInRoutingModule
+  ]
+})
+export class SingInModule { }
---
+++ src/app/auth/sing-up/sing-up.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SingUpRoutingModule } from './sing-up-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SingUpRoutingModule
+  ]
+})
+export class SingUpModule { }
